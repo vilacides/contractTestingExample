a0e41f818294060cfe1b888aaa658c7eb6e0f0e0
Isa Vilacides <m.vilacides@gmail.com>
2018-04-17 18:26:50 +0200

    Initial commit with the sample controller and consumer as well as the tests and pact file

---
+++ src/phonebook-consumer/src/main/java/cloudbees/phonebook/PhoneNumber.java
@@ -0,0 +1,7 @@
+package cloudbees.phonebook;
+
+/**
+ * Created by mariaisabelmunozvilacides on 07/03/2018.
+ */
+public class PhoneNumber {
+}
---
+++ src/phonebook-provider/src/main/java/cloudbees/phonebook/PhonebookController.java
@@ -0,0 +1,35 @@
+package cloudbees.phonebook;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by mariaisabelmunozvilacides on 08/03/2018.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class PhonebookController {
+
+    private final Map<String, String> phoneDirectory;
+
+    public PhonebookController() {
+        phoneDirectory = new HashMap<>();
+        phoneDirectory.put("mum", "+34684088275");
+    }
+
+    @RequestMapping("/{name}")
+    @ResponseBody
+    String giveAPhone(@PathVariable String name) {
+        return phoneDirectory.get(name);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PhonebookController.class, args);
+    }
+}
